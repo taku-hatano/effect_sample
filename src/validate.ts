d01728b4781a64cf3e4ef792f141491b0edbc1ea
hatano <taku_hatano@forcia.com>
2024-05-15 16:42:55 +0900

    fix sample

--- src/validate.ts
+++ src/validate.ts
@@ -20,10 +20,18 @@ export class ValidationError extends Data.TaggedError("ValidationError")<{
 }
 
 export function validateParams(params: unknown) {
+	const result = schema.safeParse(params);
+	if (!result.success) {
+		// エラー発生時にはEffect.failを使ってエラーを返す
+		return Effect.fail(ValidationError.fromZodError(result.error));
+	}
+	return Effect.succeed(result.data);
+}
+
+export function validateParamsGen(params: unknown) {
 	return Effect.gen(function* () {
 		const result = schema.safeParse(params);
 		if (!result.success) {
-			// エラー発生時にはEffect.failを使ってエラーを返す
 			return yield* Effect.fail(ValidationError.fromZodError(result.error));
 		}
 		return result.data;
